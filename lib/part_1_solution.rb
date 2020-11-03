0fdc519ba69ffa8ce41720215e28143180334817
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-03 00:35:53 +0000

    Done.

--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
   index = 0
 
@@ -11,24 +12,20 @@ end
 
 
 def consolidate_cart(cart)
-  index = 0
   new_cart = []
   
   cart.each do |grocery_item|
     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
     if current_item
-      new_cart_index = 0
       new_cart.each do |new_cart_item|
         if new_cart_item[:item] === current_item[:item]
           new_cart_item[:count] += 1
         end
-        new_cart_index += 1
       end
     else
       grocery_item[:count] = 1
       new_cart << grocery_item
     end
-    index += 1
   end
   new_cart
 end
